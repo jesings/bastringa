efebad067bbf55eab0663465a8111fffe88c9023
Theodore Peters <tpeters@stuy.edu>
2018-09-28 22:05:18 -0400

    !=0 smh my head

--- bastr.c
+++ bastr.c
@@ -12,7 +12,7 @@ char* strcat(char* dest,char* source){
 }
 int strcmp(char* s1, char* s2){
     int i = 0;
-    for(;s1[i]!=0;i++){
+    for(;s1[i];i++){
         if(!s2[i]) return 1; 
         if(s1[i]>s2[i]) return 1;
         else if(s1[i]<s2[i]) return -1;
