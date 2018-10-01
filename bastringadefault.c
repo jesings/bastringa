4d74624523497a684f787e6b3a4e2024e0ba85ca
jesings <34111484+jesings@users.noreply.github.com>
2018-10-01 10:08:01 -0400

    Create bastringadefault.c

---
+++ bastringadefault.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(){
+  printf("Builtin functions:\n");
+  unsigned char b[16] = "lost my ";
+  unsigned char g[16] = "lost my ";
+  unsigned char j[16] = "marbles";
+  printf("g is %s, and j is %s\n",g,j);
+  strcat(g,j);
+  printf("g and j concatenated is %s\n", g);
+  printf("changing g back to \"lost my\" (but now sans space): %s\n",strncpy(g,b,7));
+  printf("g is %d characters long\n", strlen(g));
+  printf("checking whether g or j is alphanumerically first: %d\n",strcmp(g,j));
+  printf("checking whether j or g is alphanumerically first: %d\n",strcmp(j,g));
+  printf("checking whether j or \"marbles\" is alphanumerically first: %d\n",strcmp(j,"marbles"));
+  char* pnotyeti = strchr(j,'b');
+  printf("checking location of \"b\" within j %x\n",pnotyeti);
+  printf("checking value at that address in j: %c\n",*pnotyeti);
+  char* pnotyet = strstr(g,"my");
+  printf("checking location of \"my\" within g %x\n",pnotyet);
+  printf("checking value at that address in g: %c%c\n",*pnotyet,*(pnotyet+1));
+  return 0;
+}
